2bc265a1ec83d21a8e70596f01ea20bfd686f45c
sprog-man <2484511957@qq.com>
2026-07-26 21:56:51 +0800

    完成进度：阶段--05-day03的问题已解决

--- ai-code-mother/src/main/java/com/xiaoliu/aiCodeMother/ai/model/HtmlCodeResult.java
+++ ai-code-mother/src/main/java/com/xiaoliu/aiCodeMother/ai/model/HtmlCodeResult.java
@@ -4,11 +4,13 @@ package com.xiaoliu.aiCodeMother.ai.model;
 
 import dev.langchain4j.model.output.structured.Description;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * HTML 代码生成结果
  */
 @Data
+@NoArgsConstructor
 public class HtmlCodeResult {
 
 //    @Description("生成的 HTML 代码")  //`@Description` 告诉 AI 这个字段的含义，帮助 AI 生成正确的 JSON：
